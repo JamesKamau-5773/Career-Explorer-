b93a16d7c57255d84b7bffedf61db8700fbaec34
roy9 <roymurimi527@gmail.com>
2025-07-22 15:16:00 +0300

    feat: add CareerForm component to collect grade, education level, and course input

--- src/components/CareerForm.jsx
+++ src/components/CareerForm.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
 
-const CareerForm = () => {
-  return (
-    <div>CareerForm</div>
+function CareerForm({updateSearchCriteria}) {
+  const [formData, setFormData] = useState({
+    grade: '',
+    educationLevel: '',
+    course: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev)=>({...prev, [name]: value }));
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateSearchCriteria(formData);
+  };
+ 
+
+    </div>
+  
+
   )
 }
 
