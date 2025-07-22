16c81eb1b842012d4db467fe4a3ac1ee5b7cf110
cynthia <cynthialairumbe@gmail.com>
2025-07-22 16:02:50 +0300

    feat:career card

--- src/components/CareerCard.jsx
+++ src/components/CareerCard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const CareerCard = () => {
+function CareerCard({career,onEdit,onDelete}) {
+  const { id, title, description, salaryRange, requiredSkills,educationalLevel,requiredGrades } = career;
   return (
-    <div>CareerCard</div>
+    <div className='Card'>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <p>Salary Range: {salaryRange}</p>
+      <p>Required Skills: {requiredSkills.join(', ')}</p>
+      <p>Educational Level: {educationalLevel}</p>
+      <p>Required Grades: {requiredGrades.join(', ')}</p>
+      <div className='CardButtons'>
+        <button onClick={() => onEdit(id)}>Edit</button>
+        <button onClick={() => onDelete(id)}>Delete</button>
+    </div>  
   )
 }
 
